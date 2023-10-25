d0542ba34182691d236035b03985f60e5d490d3f
Hugo McNally <hugo.mcnally@gmail.com>
2023-10-25 16:12:19 +0100

    Added lowrisc toolchain to flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "Machine Learning for Digital Verification";
 
-  outputs = { self, nixpkgs }: let
+  inputs = {
+    toolchains.url = "github:HU90m/lowrisc-toolchains/nix";
+  };
+
+  outputs = { self, nixpkgs, toolchains }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    toolchain = toolchains.packages.x86_64-linux.default;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
         packages = with pkgs; [
+          toolchain
           zlib
           verilator
           python311
@@ -15,6 +21,25 @@
           python311Packages.flake8
           python311Packages.mypy
         ];
+    };
+    apps.x86_64-linux.asm2bytes = let
+      asm2bytes = pkgs.writeShellApplication {
+        name = "asm2bytes";
+        runtimeInputs = [ toolchain ];
+        text = ''
+          ASM_FILE="$1"
+          OBJ_FILE="$(mktemp)"
+          BYTE_FILE="$2"
+
+          echo "Assembling..."
+          riscv32-unknown-elf-as "$ASM_FILE" -o "$OBJ_FILE"
+          echo "Extracting bytes..."
+          riscv32-unknown-elf-objcopy -O binary --only-section=.text "$OBJ_FILE" "$BYTE_FILE"
+        '';
+      };
+    in {
+      type = "app";
+      program = "${asm2bytes}/bin/asm2bytes";
     };
   };
 }
